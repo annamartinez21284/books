5556af802265f285083e699fc558c47f53d643ee
Anna <annamartinez0212@gmail.com>
2020-01-25 14:25:08 +0800

    1st commit books

---
+++ application.py
@@ -0,0 +1,57 @@
+import os
+
+from flask import Flask, session, request, render_template
+from flask_session import Session
+#from flask_sqlalchemy import SQLAlchemy # I added this I think
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+from werkzeug.security import check_password_hash, generate_password_hash
+
+app = Flask(__name__)
+
+# I added below line
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("postgres://otdgotgwpmsmhr:f88d0e15de2c7ee58e196e140accad01a7ccb16fd3e994734419e197a709d1ca@ec2-79-125-126-205.eu-west-1.compute.amazonaws.com:5432/df6uopvbd3t6ii")
+
+# Check for environment variable
+if not os.getenv("DATABASE_URL"):
+    raise RuntimeError("DATABASE_URL is not set")
+
+# Configure session to use filesystem
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Set up database
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
+
+
+@app.route("/", methods=["GET", "POST"])
+@login_required
+def index():
+  session.clear()
+  return render_template("index.html")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+  session.clear()
+  if request.method == "POST":
+    user = request.form.get("username")
+    if user == None:
+      return render_template("apology.html", text="Please choose username.")
+
+    if not request.form.get("password") or not request.form.get("confirmation"):
+      return render_template("apology.html", text="Pls provide a password and confirm it.")
+    hash = generate_password_hash(request.form.get("password"), method='pbkdf2:sha256', salt_length=8)
+    if not check_password_hash(hash, request.form.get("confirmation")):
+      return render_template("apology.html", text="Password and confirmation do not match.")
+
+    try: # how do I know whicherror to catch if I dont know which error will occur? duplication, connection error, syntax error...?
+      db.execute("INSERT INTO users (name, hash) VALUES (:name, :hash)", {"name": name, "hash": hash})
+      db.commit()
+    except:
+      return render_templat("apology.html", text="User already exists or something else wrong.")
+    session["username"] = user
+    return render_template("index.html")
+
+    session
---
+++ create.py
@@ -0,0 +1,20 @@
+import os
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker # Not sure I need scoped_session for create.py, users don't mess with this..?
+
+# create an engine
+# Note: os.getenv("...") is used to get value of env variable (&returns connection string). Below is a connection string though.
+# create_engine() then expects a connection string as argument.
+engine = create_engine("postgres://otdgotgwpmsmhr:f88d0e15de2c7ee58e196e140accad01a7ccb16fd3e994734419e197a709d1ca@ec2-79-125-126-205.eu-west-1.compute.amazonaws.com:5432/df6uopvbd3t6ii")
+
+# Create a configured Session-class
+Session = sessionmaker(bind=engine)
+
+# create a session
+db = Session()
+
+db.execute("CREATE TABLE users (id SERIAL PRIMARY KEY, name VARCHAR NOT NULL, hash VARCHAR NOT NULL)")
+db.execute("CREATE TABLE books (isbn PRIMARY KEY, title VARCHAR NOT NULL, author VARCHAR, year INTEGER)")
+db.execute("CREATE TABLE reviews (id SERIAL PRIMARY KEY, rating INTEGER, user REFERENCES users, isbn REFERENCES books, CHECK (rating > 0 AND rating < 6))")
+db.commit()
